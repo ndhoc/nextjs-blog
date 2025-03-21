45d96340a93c5dcaf0e1886c91c06183604a93c3
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-03-21 19:04:52 +0700

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,6 +3,8 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const MAX_DISPLAY = 5
 
