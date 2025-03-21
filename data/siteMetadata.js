68dd9b9cc04f3e838cce37713f142bbe68be7ca4
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-03-21 19:20:18 +0700

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://ndhoc.com',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/ndhoc/nextjs-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/',
